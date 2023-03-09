dee61264d1525e9d2f3a3316f94808d37f119ab3
root <iamsm4rt000@gmail.com>
2023-03-09 06:30:19 +0000

    Updated to lastest version of pyrogram

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# MasihSelf
-
+# MasihSelf V2.2
+created by https://github.com/Masihgh/MasihSelf && animations increased by : https://github.com/SM4RT0/MasihSelf/
 ## How Usage
-* [0] Clone or Fork Source
-* [1] pip: -r requirements.txt
-
-* [3] delete ``-example`` in config.ini
-* [4] Go to [https://my.telegram.org]
-* [5] Create API 
-* [6] Get ``API Key`` And ``API Hash`` and replace in ``config.ini``
-* [7] if use proxy in system set True Proxy And Config
-* [8] run ``main.py``
-* [9] type your number : ``+98912345678``
-* [10] type telegram code
-* [11] rerun ``main.py``
-* [12] if edit codes and plugins send ``Reload`` in any chat!
-* [13] send ``help`` and use Bot~
+    [0] git clone https://github.com/SM4RT0/MasihSelf.git
+    [1] cd MasihSelf && pip install -r requirements.txt
+    [2] Open main.py in your favorite text editor.
+    [3] Look for the lines that say api_id and api_hash.
+    [4] Replace the placeholders with your actual API key and hash, obtained from [https://my.telegram.org].
+    [5] If you use a proxy, look for the lines that say proxy_addr, proxy_port, proxy_username, and proxy_password and replace the placeholders with your actual proxy configuration.
+    [6] Save and close the file.
+    [7] Run python3 main.py to start the bot.
+    [8] Type your phone number when prompted (e.g., +98912345678).
+    [9] Type the verification code you receive from Telegram.
+    [10] Rerun python3 main.py to restart the bot with your updated configuration.
+    [11] If you edit the codes and plugins, send Reload in any chat to reload the bot.
+    [12] Send help and use the bot.
+-----------------------------------------------------------------------------------------------------
--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-import re, os, importlib
+import re
+import os
+import importlib
 from pyrogram import Client
 from pyrogram.client import Client
 from pyrogram.types.messages_and_media.message import Message
 from pyrogram import filters
 
-client = Client('user', config_file='config.ini')
+plugins = dict(root="plugins")
 
-VERSION = 1.0
+# Enter your API ID and hash here
+api_id = 123456
+api_hash = "xxxxxxxxxxx"
 
+# Enter your proxy configuration here (if applicable)
+use_proxy = False
+proxy_addr = "proxy_address"
+proxy_port = "proxy_port"
+proxy_username = "proxy_username"
+proxy_password = "proxy_password"
 
+# Create a Pyrogram client instance
+if use_proxy:
+    client = Client(
+        "my_account",
+        plugins=plugins,
+        api_id=api_id,
+        api_hash=api_hash,
+        proxy=dict(
+            hostname=proxy_addr,
+            port=proxy_port,
+            username=proxy_username,
+            password=proxy_password,
+        )
+    )
+else:
+    client = Client(
+        "my_account",
+        plugins=plugins,
+        api_id=api_id,
+        api_hash=api_hash
+    )
 
+# Define some constants
+VERSION = 2.0
 
-
-
+# Define some Pyrogram handlers
 @client.on_message(filters.regex('^version$', re.I) & filters.me)
 async def version(client: Client, message: Message):
     await message.edit("Version {}".format(VERSION))
 
 @client.on_message(filters.regex('^help$', re.I) & filters.me)
-async def help(client: Client, message: Message):
+async def help_command(client: Client, message: Message):
     help = """
     version: Return current version
     help: Return this help message
@@ -35,4 +67,5 @@ async def help(client: Client, message: Message):
     i = 0
     await message.edit("HELP : \n{}\n version:{}".format(help, VERSION))
 
+# Start the Pyrogram client
 client.run()
